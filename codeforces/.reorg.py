ca37d06a3a79767a3c4d109556bf061eaa68dcba
Josip <josips@live.ca>
2023-02-13 18:40:45 -0500

    Delete .reorg.py

--- codeforces/.reorg.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import os
-
-for root, dirs, files in os.walk('.'):
-  for dir in dirs:
-    print(f'{root}/{dir}')
-    shutil.rmtree(f'{root}/{dir}')
-  # for file in files:
-    # shutil.move(f'{root}/{file}', f'{root}{file}')
